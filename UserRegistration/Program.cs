9d9cc37a2b8168a529aaa99936fa910c67c8ba74
Prasad <banprasad21@gmail.com>
2022-08-29 18:23:51 +0530

    [Raghav] Add: Regex user first name validation

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserValidation userRegexCheck = new UserValidation();
+            string FirstName = "Raghav";
+            userRegexCheck.ValidateFirstName(FirstName);
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class UserValidation
+    {
+        public Regex FirstNameRegex = new Regex(@"^[A-Z]{1}[a-zA-Z]{2,}$");
+        public void ValidateFirstName(string FirstName)
+        {
+            Console.WriteLine("\nFirst name: " + FirstName);
+            if (FirstNameRegex.IsMatch(FirstName))
+                Console.WriteLine("Valid First Name");
+            else
+                Console.WriteLine("Invalid First Name");
+        }
+    }
+}
